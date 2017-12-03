5537c2621b9cb6d3f23ace8ea58c4370d17b5f2c
Gilad Ram <giladram@gmail.com>
2017-12-03 19:24:34 +0200

    New NodeJs branch

---
+++ JavaScript/Exersice_7.js
@@ -0,0 +1,14 @@
+
+ process.stdin.setEncoding('utf8');
+
+process.stdout.write("Enter a text to rotate: ");
+process.stdin.on('data', function (text) {
+	pureText = text.replace('\n','');
+	process.stdout.write(pureText);
+    for(var i = 0; i < pureText.length - 1; i++){
+    	currentLastLetter = pureText[pureText.length -1];
+    	pureText = currentLastLetter + pureText.slice(0, pureText.length - 1 );
+    	console.log(pureText)
+    }
+    process.exit();
+});
