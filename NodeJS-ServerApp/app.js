15adc3794da3e87928f29da8bd67a959bd95961b
Gilad Ram <giladram@gmail.com>
2017-12-20 17:46:53 +0200

    nodejs time and file api

---
+++ NodeJS-ServerApp/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const fs = require('fs');
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.get('/gettime', (req,res) => {
+	let date = new Date();	
+	let currentTime = date.toTimeString();
+	res.send("The time current Time on the server: " + currentTime);
+})
+
+app.get('/getFile', (req,res) => {
+	let reqFileName = req.query.filename || "No filename"
+	let fullFileName = reqFileName + '.txt' 
+	fs.readFile( fullFileName ,(err, content) => {
+	if (err) {
+ 		console.error(err);
+ 		res.send("Could not find file name: " + reqFileName)
+ 		return;
+		}
+ 	console.log("Got content with length - " + content.length);
+ 	res.send(content.toString())
+	});
+})
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
