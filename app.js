50fe61dfc24c3c8120f3d72a01e5c319be2a1b4e
Gilad Ram <giladram@gmail.com>
2017-12-20 18:31:21 +0200

    change port to const

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const fs = require('fs');
-let port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
