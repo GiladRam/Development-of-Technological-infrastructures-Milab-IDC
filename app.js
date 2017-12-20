d7c2d3e536179c06fa80f7f00a178a1658b08b78
Gilad Ram <giladram@gmail.com>
2017-12-20 18:27:16 +0200

    change the nodejs server port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const fs = require('fs');
+let port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -26,4 +27,4 @@ app.get('/getFile', (req,res) => {
 })
 
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('Example app listening on port' + port + '!'))
