3718d9c1527cc9fbc7b45e2afb44958755c605a3
Gilad Ram <giladram@gmail.com>
2018-01-10 17:27:44 +0200

    api kibana

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const fs = require('fs');
 const port = process.env.PORT || 8080;
+let http = require('http');
+let server = http.createServer(app); 
+let io = require('socket.io')(server);
+let mainClient = null;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Welcome!!! MOLO API'))
 
 app.get('/gettime', (req,res) => {
 	let date = new Date();	
 	let jsonCurrentTime = { currentTime: date.toJSON()};
-	res.send(jsonCurrentTime);
+	res.send(jsonurCrentTime);
 })
 
 app.get('/getFile/:filename', (req,res) => {
@@ -20,5 +24,41 @@ app.get('/getFile/:filename', (req,res) => {
 		res.send("Could not find file " + filePath);		
 	}	
 })
+
+io.on('connection', function(client) {  
+    console.log('Client connected...');
+    mainClient = client;
+    console.log(mainClient);
+
+    client.on('join', function(data) {
+        console.log(data);
+    });
+
+});
+
+
+var options = {
+  url: 'http://52.214.115.168:9200',
+  path: '/pi-log*/_search',
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json',
+  }
+};
+
+
+setInterval(function () { 
+	var req = http.request(options, function(res) {
+  		console.log('Status: ' + res.statusCode);
+  		console.log('Headers: ' + JSON.stringify(res.headers));
+  		res.setEncoding('utf8');
+  		res.on('data', function (body) {
+    	console.log('Body: ' + body);
+  			});
+		});  
+		req.on('error', function(e) {
+  			console.log('problem with request: ' + e.message);
+		});   
+}, 1000); 
 
 app.listen(port, () => console.log('Example app listening on port' + port + '!'))
